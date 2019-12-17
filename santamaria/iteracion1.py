259e580a6938ec7247917d8178121f9a8614f16f
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-17 08:36:29 -0500

    Commit iteracion1

---
+++ santamaria/iteracion1.py
@@ -0,0 +1,15 @@
+#Iteración1
+
+import os
+msg=os.sys.argv[1]
+
+for letra in msg:
+
+    if(letra=="A"):
+        print("HOLA")
+    if(letra=="B"):
+        print("MI AMOR")
+    if(letra=="C"):
+        print("COMO")
+    if(letra=="D"):
+        print("ESTAS?")
