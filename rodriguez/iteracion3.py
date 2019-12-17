fbc6341a6d1625628f996b6e29b7eeee223cf0a6
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-17 10:41:23 -0500

    Commit iteracion3

---
+++ rodriguez/iteracion3.py
@@ -0,0 +1,15 @@
+#Iteracion
+import os
+
+msg=str(os.sys.argv[1])
+
+for letra in msg:
+    if(letra == "A"):
+        print("aaaa")
+    if(letra == "B"):
+        print("234")
+    if(letra == "C"):
+        print("baiss")
+    if(letra == "D"):
+        print("raiz")
+#finfor
