f415165ed6c356ae674eb5af622f6625461ec432
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-17 10:49:45 -0500

    Commit rango2

---
+++ rodriguez/Rango2.py
@@ -0,0 +1,8 @@
+anchura = int(input("Anchura del rectángulo: "))
+altura = int(input("Altura del rectángulo: "))
+
+for i in range(altura):
+    for j in range(anchura):
+        print("* ", end="")
+    print()
+#fin_for
