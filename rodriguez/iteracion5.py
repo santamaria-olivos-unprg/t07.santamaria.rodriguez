dd3397edc8cbc59ab9d4f19cc1eb8bb39c8e18f9
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-17 10:42:25 -0500

    Commit iteracion5

---
+++ rodriguez/iteracion5.py
@@ -0,0 +1,15 @@
+#Iteracion
+import os
+
+msg=str(os.sys.argv[1])
+
+for letra in msg:
+    if(letra == "x"):
+        print("the")
+    if(letra == "y"):
+        print("climb")
+    if(letra == "z"):
+        print("by")
+    if(letra == "w"):
+        print("Miley")
+#finfor
