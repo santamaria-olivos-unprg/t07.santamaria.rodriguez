ebcf14039af04ec9940d8a18f20cbc46b2fd48b3
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-17 10:54:19 -0500

    Commit lote.bat

---
+++ lote.bat
@@ -0,0 +1,34 @@
+python santamaria/iteracion1.py ABCD
+python santamaria/iteracion1.py AAAA
+python santamaria/iteracion1.py BBB
+python santamaria/iteracion2.py AEIOU
+python santamaria/iteracion2.py EEEE
+python santamaria/iteracion2.py UUU
+python santamaria/iteracion3.py ****
+python santamaria/iteracion3.py *#*#
+python santamaria/iteracion3.py #$*
+python santamaria/iteracion4.py ***
+python santamaria/iteracion4.py *+#$-
+python santamaria/iteracion4.py ---
+python santamaria/iteracion5.py 12345
+python santamaria/iteracion5.py 345
+python santamaria/iteracion5.py 5555
+python Rodriguez/iteracion01.py GARF
+python Rodriguez/iteracion01.py GFRA
+python Rodriguez/iteracion01.py ARFG
+
+python Rodriguez/iteracion02.py EDJF
+python Rodriguez/iteracion02.py DEFJ
+python Rodriguez/iteracion02.py FEDJ
+
+python Rodriguez/iteracion03.py DACB
+python Rodriguez/iteracion03.py ADCB
+python Rodriguez/iteracion03.py ABCD
+
+python Rodriguez/iteracion04.py MODE
+python Rodriguez/iteracion04.py DEMO
+python Rodriguez/iteracion04.py EOMD
+
+python Rodriguez/iteracion05.py xyzw
+python Rodriguez/iteracion05.py xwzy
+python Rodriguez/iteracion05.py wxzy
