d99f42d7bd5658ac444255a3b11148f7dcb94532
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-17 10:39:28 -0500

    Commit iteracion1

---
+++ rodriguez/iteracion1.py
@@ -0,0 +1,15 @@
+#Iteracion
+import os
+
+msg=str(os.sys.argv[1])
+
+for letra in msg:
+    if(letra == "G"):
+        print("Grethell")
+    if(letra == "A"):
+        print("Abigail")
+    if(letra == "R"):
+        print("Rodriguez")
+    if(letra == "F"):
+        print("Fiestas")
+#finfor
