ade5b153698f2679cdd0d448dbc59c1cf644b1b3
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-16 22:36:55 -0500

    Commit for3

---
+++ santamaria/for3.py
@@ -0,0 +1,13 @@
+#Programa que muestre los números del -49 hasta el 51
+i=-50
+x=50
+
+while(i<=50):
+    i=i+1
+    print(i)
+print("Programa de menor a mayor")
+while(x>-51):
+    x=x-1
+    print(x)
+#fin_while
+print("Fin de programa de mayor a menor")
