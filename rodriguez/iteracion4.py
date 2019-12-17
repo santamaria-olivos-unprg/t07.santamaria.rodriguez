da881eedf57022629f57526b47f055d29f400371
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-17 10:42:05 -0500

    Commit iteracion4

---
+++ rodriguez/iteracion4.py
@@ -0,0 +1,15 @@
+#Iteracion
+import os
+
+msg=str(os.sys.argv[1])
+
+for letra in msg:
+    if(letra == "D"):
+        print("1234")
+    if(letra == "E"):
+        print("triangulo")
+    if(letra == "M"):
+        print("nana")
+    if(letra == "O"):
+        print("mimi")
+#finfor
