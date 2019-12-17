3681c938c58abc0b63b2bd6682d07534cef1b19a
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-17 08:38:25 -0500

    Commit iteracion5

---
+++ santamaria/iteracion5.py
@@ -0,0 +1,15 @@
+#iteración
+import os
+msg=os.sys.argv[1]
+
+for numero in msg:
+    if(numero=="1"):
+        print("TENGO")
+    if(numero=="2"):
+        print("QUE COMPRAR")
+    if(numero=="3"):
+        print("FRUTAS")
+    if(numero=="4"):
+        print("O")
+    if(numero=="5"):
+        print("CHOCOLATES")
