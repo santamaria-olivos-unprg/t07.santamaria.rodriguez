1d7e72316d380bc3f9a36eeec00d8cd7761b7b1f
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-17 10:45:10 -0500

    Commit while3

---
+++ rodriguez/while3.py
@@ -0,0 +1,8 @@
+usuario=""
+usuario_invalidado=(usuario!="garf")
+while(usuario_invalidado):
+      usuario=input("Ingrese usuario:")
+      usuario_invalidado=(usuario!="garf")
+
+#finwhile
+print("Acceso Concedido")
