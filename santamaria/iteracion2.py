0c3d788125196612fa2ad376682b098226090ac9
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-17 08:36:56 -0500

    Commit iteracion2

---
+++ santamaria/iteracion2.py
@@ -0,0 +1,16 @@
+#Iteración2
+
+import os
+msg=os.sys.argv[1]
+
+for vocal in msg:
+    if(vocal=="A"):
+        print("NECESITO")
+    if(vocal=="E"):
+        print("URGENTE")
+    if(vocal=="I"):
+        print("UNA CALCULADORA")
+    if(vocal=="O"):
+        print("PARA")
+    if(vocal=="U"):
+        print("HOY")
