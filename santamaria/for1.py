84142c16133b11e174bc97365c48fc8d41ea5685
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-16 22:35:25 -0500

    Commit for1

---
+++ santamaria/for1.py
@@ -0,0 +1,12 @@
+#Programa que muestre los números impares del 0 al 501
+a=0
+max=501
+
+while(a<=max):
+    a=a+1
+    if((a%2)!=0):
+        print(a)
+    #fin_if
+
+#fin_while
+print("Fin del programa de los números impares")
