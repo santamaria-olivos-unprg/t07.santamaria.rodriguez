a7744a682d01be76a0e4298cd3b98507c1e50328
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-17 08:38:55 -0500

    Commit rango1

---
+++ santamaria/rango1.py
@@ -0,0 +1,10 @@
+#Programa que muestre los números impares del 1 al 501
+a=0
+for a in range(502):
+    a=a+1
+    if((a%2)!=0):
+        print(a)
+    #fin_if
+
+#fin_while
+print("Fin del programa de los números impares")
